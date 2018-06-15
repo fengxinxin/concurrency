616fea495bc9b987f3cd01b7c39b65df35ee62a0
iOLO <516502968@qq.com>
2018-06-15 16:50:37 +0800

    添加semaphore和countDownLatch

---
+++ src/main/java/com/iolo/concurrency/ConcurrencyTest.java
@@ -0,0 +1,45 @@
+package com.iolo.concurrency;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by FengXinXin on 2018/6/15.
+ */
+public class ConcurrencyTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrencyTest.class);
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+    public static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Semaphore semaphore = new Semaphore(threadTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    LOGGER.error(e.getMessage(), e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        LOGGER.info("Count : {}",count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
